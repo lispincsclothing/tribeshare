06bb3444d666e7f9394fd4dcea6e9cee6f8ea9bb
lispincsclothing <scullingnull+1@gmail.com>
2015-11-07 18:05:30 -0500

    add webhook json & crsf change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # remove below for webhooks
+  # protect_from_forgery with: :exception
 end
