b12f50b828aab5afb74ef04ce8996b523793379e
grandnumber <thegrandnumber@gmail.com>
2015-11-07 22:49:50 -0500

    seed1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Campaign.create!(header:'MILLI MILLU HANDBAG',
+cost:750.00,
+title:'Amazing handbag from London',
+picture1:'millimillu.jpg',
+picture2:'stockholm.jpg',
+picture1_title:'The Stockholm by MilliMillu',
+picture2_title: 'Stockholm bag',
+description:'I love this bag!!!It is a startup founded by a former management consultant - not please keep reading!!! ;-) Anyway like many of us she struggled to find chic, but practical handbags for work - these look awesome. But at near 500 pounds and iternational availability only it is a big risk to buy one. So I would love for us to try it out. I figure if we start with a month each we can find out whether it is worth investing more heavily... I am willing to pay more to have the bag for longer if people want - let me know!!! xxx Ram
+Check out the website, Oooh and not that it matters (ahem) but Elle McPherson loves this bag!!!!',
+expiration:Date.today+1.week,
+minimum_contribution:100.00,
+owner_id:1,
+custodian_id:1,
+rfid:'tbd')
