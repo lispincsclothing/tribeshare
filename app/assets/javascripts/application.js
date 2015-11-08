f4343d7e73743572dcd5f273876ef2789befb878
grandnumber <thegrandnumber@gmail.com>
2015-11-07 20:51:59 -0500

    add bootstrap et al require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap.min
+//= require jquery.waypoints.min
+//= require jquerycountto
+//= require lightbox.min
