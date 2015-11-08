fcc9a33d69048b17650a5684d7275c82609e343d
grandnumber <thegrandnumber@gmail.com>
2015-11-07 21:58:18 -0500

    homepage base

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -69,6 +69,6 @@ class HooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def hook_params
-      params.require(:hook).permit(:rfid, :photon)
+      params.require(:hook).permit(:rfid, :photon_id)
     end
 end
