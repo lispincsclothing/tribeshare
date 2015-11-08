be1e5f0dabc028e6c3565362f3ac8278c7d32da8
lispincsclothing <scullingnull+1@gmail.com>
2015-11-08 08:55:59 -0500

    Fix

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -29,11 +29,9 @@ class HooksController < ApplicationController
     puts "DEBUG_HOOKSCONTROLLER ", Campaign.where(rfid:params["hook"]["rfid"].to_s), User.where(photon:params["hook"]["photon"].to_s)
     # @hook = Hook.new(rfid:params["hook"]["rfid"], photon:params["hook"]["photon"])
     # # {"event"=>"handover_update6", "data"=>"2bc9851e", "published_at"=>"2015-11-08T13:08:00.563Z", "coreid"=>"360039000a47343432313031", "hook"=>{"rfid"=>"2bc9851e", "photon"=>"360039000a47343432313031"}, "controller"=>"hooks", "action"=>"create"}
-    Campaign.where(rfid:params["hook"]["rfid"].to_s) do |campaign|
-      custody = campaign.custodian
-      custody.custodian_id=User.where(photon:params["hook"]["photon"].to_s).first!.owner_id
-      campaign.save!
-    end
+    custody = Campaign.where(rfid:params["hook"]["rfid"].to_s).first
+    custody.custodian_id=User.where(photon:params["hook"]["photon"].to_s).first!.user_id
+    campaign.save!
 
     respond_to do |format|
       if @hook.save
